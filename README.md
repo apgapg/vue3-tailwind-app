c5ce3ed0c413b4bcca24f080864270174167c0b0
Ayush P Gupta <ayushpguptaapg@gmail.com>
2021-12-25 10:17:33 +0530

    Initial Commit
    Signed-off-by: Ayush P Gupta <ayushpguptaapg@gmail.com>

--- README.md
+++ README.md
@@ -1,24 +1,17 @@
 # vue3-tailwind-app
 
-## Project setup
-```
-npm install
-```
+Demonstration of using Tailwindcss in Vue3.
 
-### Compiles and hot-reloads for development
-```
-npm run serve
-```
+### Note
+ Currently Tailwindcss 3 isn't supported in Vue3 due to postcss 8 incompatibility. Hence we are using `npm:@tailwindcss/postcss7-compat` or Tailwindcss v2
 
-### Compiles and minifies for production
-```
-npm run build
-```
+<img src="./res/s1.png"/>
 
-### Lints and fixes files
-```
-npm run lint
-```
+## Getting Started
+
+1. Clone respository
+2. Run `npm install`
+3. Run `npm run serve`
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,52 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div class="bg-purple-500 min-h-screen text-white px-16 flex flex-col justify-around py-4">
+    <div>
+      <div class="text-white text-xl text-center">This is a Vue3 + Tailwindcss demo</div>
+      <div class="text-white text-xl pt-4 text-center">Made with ❤️ by Ayush P Gupta</div>
+    </div>
+
+    <div class="flex justify-around">
+      <div class="pt-8">
+        <div>1. Install Vue CLI</div>
+        <pre class="bg-gray-800 w-min text-start p-8 mt-4 rounded shadow-xl"><code class="text-start">npm install -g @vue/cli
+# OR
+yarn global add @vue/cli</code></pre>
+      </div>
+      <div class="pt-8">
+        <div>2. Create Vue Project</div>
+        <pre class="bg-gray-800 w-min text-start p-8 mt-4 rounded shadow-xl"><code class="text-start">vue create my-app</code></pre>
+      </div>
+      <div class="pt-8">
+        <div>3. Add Tailwindcss (npm:@tailwindcss/postcss7-compat)</div>
+        <pre class="bg-gray-800 w-min text-start p-8 mt-4 rounded shadow-xl"><code class="text-start">vue add tailwindcss</code></pre>
+      </div>
+    </div>
+    <div class="flex justify-around">
+      <div class="pt-8">
+        <div>4. Generate config file</div>
+        <pre class="bg-gray-800 w-min text-start p-8 mt-4 rounded shadow-xl"><code class="text-start">npx tailwindcss init -p</code></pre>
+      </div>
+      <div class="pt-8">
+        <div>5. Import tailwindcss in main.js</div>
+        <pre class="bg-gray-800 w-min text-start p-8 mt-4 rounded shadow-xl"><code class="text-start">import '@/assets/tailwind.css'
+</code></pre>
+      </div>
+      <div class="pt-8">
+        <div>6. Check if Tailwind works</div>
+        <pre class="bg-gray-800 w-min text-start p-8 mt-4 rounded shadow-xl"><code class="text-start">npm run serve
+
+// Try adding a class to a div
+// like bg-red-500</code></pre>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
 
 export default defineComponent({
   name: "App",
-  components: {
-    HelloWorld,
-  },
 });
 </script>
 
@@ -20,8 +55,6 @@ export default defineComponent({
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
